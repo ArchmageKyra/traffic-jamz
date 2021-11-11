3ae34280ea41b97da5ab93a8dffebfe4ecd2758c
ArchmageKyra <kyra.slovacek@gmail.com>
2021-11-11 08:54:49 -0700

    Linked views with basic styling

--- Connected Experiments/js/markerclusterModule.js
+++ Connected Experiments/js/markerclusterModule.js
@@ -1,23 +1,61 @@
-//execute only when window is fully loaded
-window.onload = function() {
-  var map = L.map('map').setView([39.5501, -105.7821], 7);
+"use strict";
 
+$(document).ready(function() {
+  // create the leaflet map
+  var map = L.map('map').setView([39, -105.65833333], 7);
+
+  // create the background of the map, and add it to the map
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
+  //make some popups and include the text from the "description" field
+  function addPopups(feature, layer) {
+    if (feature.properties && feature.properties.Description) {
+      layer.bindPopup(feature.properties.Description);
+    }
+  }
 
-  var points = omnivore.csv('data/CO_Accidents_All.csv');
-  //points.addTo(map);
+  //customize the appearance of the markers
+  function pointToCircle(feature, latlng) {
+    var geojsonMarkerOptions = {
+      radius: 8,
+      fillColor: "yellow",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+    var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
+    return circleMarker;
+  }
 
-  var markers = L.markerClusterGroup({
-    showCoverageOnHover: false
+  //make a helper to actually apply the styling options with Omnivore
+  var omnivoreStyleHelper = L.geoJSON(null, {
+    onEachFeature: addPopups,
+    pointToLayer: pointToCircle
   });
 
-  map.addLayer(markers);
-
+  //set the default value of points to the whole CSV file, and call the style helper
+  var points = omnivore.csv('data/stupidaccidentsv4.csv', null, omnivoreStyleHelper);
+  //initialize the marker clusters
+  var clusters = L.markerClusterGroup();
   points.on('ready', function() {
-    console.log(points.getLayers().length)
-    markers.addLayer(points);
+    clusters.addLayer(points);
+
   });
-}
+  map.addLayer(clusters);
+
+  // on brushing, update the selected points with the most bass-ackwards method of deleting and re-adding layers
+  parcoords.on("brush", function() {
+    var selectedPoints = GeoJSON.parse(parcoords.brushed(), {
+      Point: ['lat', 'lng'],
+      include: ['Description']
+    });
+    points.clearLayers();
+    points.addData(selectedPoints);
+    clusters.clearLayers();
+    clusters.addLayer(points);
+  });
+
+});
--- Connected Experiments/js/parcoordsModule.js
+++ Connected Experiments/js/parcoordsModule.js
@@ -1,26 +1,32 @@
 "use strict";
-var selectedPoints;
+let lightFill = "#74D2FE";
+let medFill = "#427C97";
+let darkFill = "#162F3B";
 
-window.onload = function() {
+
+$(document).ready(function() {
   // load csv file and create the parcoords chart
   d3.csv('data/stupidaccidentsv4.csv', function(data) {
     parcoords
       .data(data)
-      .hideAxis(["lat", "lng", "Description", "Start_Time", "Civil_Twilight"])
+      .hideAxis(["lat", "lng", "hour", "month", "Description", "Start_Time", "Civil_Twilight"])
       .render()
-      .brushMode("1D-axes")
-      .reorderable();
+      .shadows()
+      .reorderable()
+      .brushMode("1D-axes");
   });
-
-  // make a color scale
-  var blue_to_brown = d3.scale.linear()
-    .domain([2, 4])
-    .range(["steelblue", "brown"])
-    .interpolate(d3.interpolateLab);
 
   // link the scale to the "Severity" property
   var color = function(d) {
-    return blue_to_brown(d['Severity']);
+    if (d['Severity'] === '4') {
+      return darkFill;
+    } else if (d['Severity'] === '3') {
+      return medFill;
+    } else if (d['Severity'] === '2') {
+      return lightFill;
+    } else {
+      return 'black';
+    }
   };
 
   // actually load the plot.  seperate from the other 'create plot' for some unexplanable reason
@@ -29,27 +35,77 @@ window.onload = function() {
     .alpha(0.4);
 
   // create the leaflet map
-  var map = L.map('map').setView([39.5501, -105.7821], 7);
+  var map = L.map('map', {
+    maxZoom: 16,
+    minZoom: 7,
+    maxBounds: [
+      //south west
+      [36.57, -109.52],
+      //north east
+      [41.45, -101.36]
+    ],
+  }).setView([39, -105.65833333], 7);
 
   // create the background of the map, and add it to the map
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  L.tileLayer('https://api.mapbox.com/styles/v1/mykmarie1/ckvsr9enk14ev15pekynxurb0/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXlrbWFyaWUxIiwiYSI6ImNrdGFvbDUzNjFvOHIydXBnbHg0eG9mbGYifQ.FrOD0LfTVItLeQU6E4ONJg', {
+    //dont forget your attribution!
+    attribution: 'Map data: &copy; <a href=”https://www.mapbox.com/about/maps/”>Mapbox</a> &copy; <a href=”http://www.openstreetmap.org/copyright”>OpenStreetMap</a>'
   }).addTo(map);
 
+  //make some popups and include the text from the "description" field
+  function addPopups(feature, layer) {
+    if (feature.properties && feature.properties.Description) {
+      layer.bindPopup(feature.properties.Description);
+    }
+  }
 
-  //set the default value of points to the whole CSV file
-  var points = omnivore.csv('data/stupidaccidentsv4.csv');
+  //customize the appearance of the markers
+  function pointToCircle(feature, latlng) {
+    //color the points based on Severity
+    let fillColorVar = "";
+    if (feature.properties.Severity === '4') {
+      fillColorVar = darkFill;
+    } else if (feature.properties.Severity === '3') {
+      fillColorVar = medFill;
+    } else if (feature.properties.Severity === '2') {
+      fillColorVar = lightFill;
+    } else {
+      fillColorVar = "black";
+    }
+
+    var geojsonMarkerOptions = {
+      radius: 8,
+      weight: 5,
+      color: fillColorVar,
+      opacity: 0.6,
+      fillColor: fillColorVar,
+      fillOpacity: 0.9
+    };
+    var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
+    return circleMarker;
+  }
+
+  //make a helper to actually apply the styling options with Omnivore
+  var omnivoreStyleHelper = L.geoJSON(null, {
+    onEachFeature: addPopups,
+    pointToLayer: pointToCircle
+  });
+
+  //set the default value of points to the whole CSV file, and call the style helper
+  var points = omnivore.csv('data/stupidaccidentsv4.csv', null, omnivoreStyleHelper);
+  //initialize the marker clusters
   var clusters = L.markerClusterGroup();
   points.on('ready', function() {
     clusters.addLayer(points);
+
   });
   map.addLayer(clusters);
 
-  // on brushing
+  // on brushing, update the selected points with the most bass-ackwards method of deleting and re-adding layers
   parcoords.on("brush", function() {
-    selectedPoints = GeoJSON.parse(parcoords.brushed(), {
+    var selectedPoints = GeoJSON.parse(parcoords.brushed(), {
       Point: ['lat', 'lng'],
-      include: ['Description']
+      include: ['Description', "Severity"]
     });
     points.clearLayers();
     points.addData(selectedPoints);
@@ -57,5 +113,4 @@ window.onload = function() {
     clusters.addLayer(points);
   });
 
-
-}
+});
